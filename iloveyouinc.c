7f73fc987ac664f88817f9db72b3f7d43285c7d8
Kazi Shakil <116259611+kazishakiil@users.noreply.github.com>
2022-10-22 01:02:05 +0600

    code by shakil

---
+++ iloveyouinc.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+    int i;
+    clrscr();
+    textcolor(RED);
+    for(i=0;i<=n;i++);
+    {
+        printf("I Love You\3");
+        delay(80);
+    }
+    }
+
+
+}
